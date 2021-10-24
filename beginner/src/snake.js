595041272f3e2544efc1a5a17151db40d2d112e9
samjtwestlake <samjtwestlake@gmail.com>
2021-10-24 17:16:58 +0100

    commit

---
+++ beginner/src/snake.js
@@ -0,0 +1,12 @@
+// identify snake length
+// identify spiralled length
+
+// if snake length - spiralled length
+// spiral until sufficient snake length contained in spiral
+
+// if spiral achieved, exit in direction of food
+// 
+
+// OR
+
+// bitmap of snake locations    
